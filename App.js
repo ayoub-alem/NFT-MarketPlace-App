5761259a2710eadb6a6093b3d4dd934fdd3a51e6
Ayoub Alem <ayoub.alem@uit.ac.ma>
2022-07-03 18:48:38 +0100

    Finishng the setup of the enivironnement and the app.js file

--- App.js
+++ App.js
@@ -18,8 +18,15 @@ const theme = {
 
 const App = () => {
   const [loaded] = useFonts({
-    
-  })
+    InterBold: require('./assets/fonts/Inter-Bold.ttf'),
+    InterRegular: require('./assets/fonts/Inter-Regular.ttf'),
+    InterSemiBold: require('./assets/fonts/Inter-SemiBold.ttf'),
+    InterLight: require('./assets/fonts/Inter-Light.ttf'),
+    InterMedium: require('./assets/fonts/Inter-Medium.ttf'),
+  });
+
+  if (!loaded) return null;
+
   return (
     <NavigationContainer theme={theme}>
       <Stack.Navigator
